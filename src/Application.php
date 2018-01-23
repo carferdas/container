2849d442099ed6af28c3de29583b4b6cf533b04a
Carlos Fernandes <carlos.fernandes.dsilva@gmail.com>
2018-01-22 22:11:15 -0400

    Add Application class

---
+++ src/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace carlosjfernandes\Container;
+
+class Application
+{
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function registerProviders(array $providers)
+    {
+        foreach ($providers as $provider) {
+            $provider = new $provider($this->container);
+            $provider->register();
+        }
+    }
+}
